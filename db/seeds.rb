ec59f35472f693b6ae655ec16bedb2b95d118fad
danajackson2 <danajackson2@gmail.com>
2020-12-14 10:43:53 -0800

    starting to be clear

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.create(coupon_code: '001', store: "Ralph's")
+Coupon.create(coupon_code: '002', store: "Von's")
+Coupon.create(coupon_code: '003', store: "QFC")
+Coupon.create(coupon_code: '004', store: "Ralph's")
+
+puts 'seeding done'
